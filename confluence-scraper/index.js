5a6943f45319df43340e82b41d85e61a871d4bc1
OMPRAKASH MISHRA <omprakashmishra3978@gmail.com>
2024-01-23 11:35:54 -0800

    code changes to support child pages.

--- confluence-scraper/index.js
+++ confluence-scraper/index.js
@@ -15,6 +15,74 @@ const server = http.createServer();
 server.listen(3002); // just to make the process does not exit.
 const BASE_URL = process.env.BASE_URL;
 const PAGE_ID_LIST = process.env.PAGES?.split(','); // expected to be comma separated page ids.
+const options = {
+  auth: {
+    username: process.env.CONFLUENCE_USER_NAME,
+    password: process.env.CONFLUENCE_TOKEN
+  }
+};
+function formatHTMLWithCheerio(html) {
+  const $ = load(html);
+  // Remove all blank elements
+  $('*').each((i, el) => {
+    if ($(el).children().length === 0 && $(el).text().trim() === '') {
+      $(el).remove();
+    }
+  });
+
+  // Remove all styles from span elements
+  $('span').replaceWith(function () {
+    return $(this).contents();
+  });
+  // remove all the styles from table elements
+  $('table').removeAttr('style');
+
+  // Get the updated HTML
+  return $.html();
+}
+async function processAPIResponse(filePath, data, isUpdated, updateIndividually, page_id, folderPath, pageTitle, sideBar, markdown) {
+  if (fs.existsSync(filePath)) {
+    const fileData = await fs_promises.readFile(filePath, 'utf-8');
+    if (fileData !== JSON.stringify(data)) {
+      if (!isUpdated) {
+        isUpdated = true;
+      }
+      updateIndividually = true;
+    } else {
+      console.info('It is already the latest version', page_id);
+    }
+  } else {
+    fs.mkdirSync(folderPath, {recursive: true});
+    updateIndividually = true;
+    if (!isUpdated) {
+      isUpdated = true;
+    }
+  }
+  if (updateIndividually) {
+    await fs_promises.writeFile(filePath, JSON.stringify(data));
+    await fs_promises.writeFile(folderPath + `/${pageTitle}.md`, sideBar + markdown);
+  }
+  return isUpdated;
+}
+async function processChildPages(folderPath, page_id) {
+  let childPagesUpdated= false;
+  const childPages = await axios.get(`${BASE_URL}/rest/api/content/${page_id}/child/page?expand=body.storage&limit=200`, options);
+  let sideBarPosition = 1;
+  if(childPages.data?.results?.length > 0) {
+    for(const childPage of childPages.data.results) {
+      const sideBar = `---\nsidebar_position: ${sideBarPosition}\n---\n`;
+      const filePath = `${folderPath}/${childPage.title}/data.json`;
+      const html = childPage?.body?.storage?.value;
+      const updatedHtml = formatHTMLWithCheerio(html);
+      const markdown = turndownService.turndown(updatedHtml);
+      const isUpdated = await processAPIResponse(filePath, childPage, false, false, childPage.id, `${folderPath}/${childPage.title}`, childPage.title, sideBar, markdown);
+      if(!childPagesUpdated && isUpdated) {
+        childPagesUpdated = isUpdated;
+      }
+    }
+  }
+  return childPagesUpdated;
+}
 async function processPageIdList() {
   const options = {
     auth: {
@@ -30,23 +98,10 @@ async function processPageIdList() {
       const sideBar = `---\nsidebar_position: ${i++}\n---\n`;
       const response = await axios.get(`${BASE_URL}/rest/api/content/${page_id}?expand=body.storage`, options);
       const folderPath = `../patterns/docs/${response.data.title}`;
+      const pageTitle = response.data.title;
       const filePath = `${folderPath}/data.json`;
       const html = response?.data?.body?.storage?.value;
-      const $ = load(html);
-      // Remove all blank elements
-      $('*').each((i, el) => {
-      if ($(el).children().length === 0 && $(el).text().trim() === '') {
-        $(el).remove();
-      }
-      });
-
-      // Remove all styles from span elements
-      $('span').replaceWith(function() {
-        return $(this).contents();
-      });
-
-      // Get the updated HTML
-      const updatedHtml = $.html();
+      const updatedHtml = formatHTMLWithCheerio(html);
       tidy(updatedHtml,  {
         indent: true,
         bare: true,
@@ -62,26 +117,10 @@ async function processPageIdList() {
           process.exit(1);
         }
         const markdown = turndownService.turndown(formattedHtml);
-        if (fs.existsSync(filePath)) {
-          const fileData = await fs_promises.readFile(filePath, 'utf-8');
-          if (fileData !== JSON.stringify(response.data)) {
-            if (!isUpdated) {
-              isUpdated = true;
-            }
-            updateIndividually = true;
-          } else {
-            console.info('It is already the latest version', page_id);
-          }
-        } else {
-          fs.mkdirSync(folderPath, {recursive: true});
-          updateIndividually = true;
-          if (!isUpdated) {
-            isUpdated = true;
-          }
-        }
-        if (updateIndividually) {
-          await fs_promises.writeFile(filePath, JSON.stringify(response?.data));
-          await fs_promises.writeFile(folderPath + `/${response.data.title}.md`, sideBar + markdown);
+        isUpdated = await processAPIResponse(filePath, response.data, isUpdated, updateIndividually, page_id, folderPath, pageTitle, sideBar, markdown);
+        const isChildPagesUpdated = await processChildPages(folderPath, page_id);
+        if(!isUpdated && isChildPagesUpdated) {
+          isUpdated = isChildPagesUpdated;
         }
 
       });
