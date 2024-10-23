273d35f3bfca523a2fe84dfaa44d79d53f803281
Jeremy Ho <jujaga@gmail.com>
2024-10-23 13:06:24 -0700

    Implement BCGov 0.4.0 Design System styles and layout
    Signed-off-by: Jeremy Ho <jujaga@gmail.com>

--- patterns/docusaurus.config.ts
+++ patterns/docusaurus.config.ts
@@ -4,8 +4,7 @@ import type * as Preset from "@docusaurus/preset-classic";
 
 const config: Config = {
   title: "NRM Architecture Patterns Library",
-  tagline:
-    "Architecture Patterns & Guidance for the Natural Resource Sector",
+  tagline: "Architecture Patterns & Guidance for the Natural Resource Sector",
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
@@ -31,14 +30,29 @@ const config: Config = {
     defaultLocale: "en",
     locales: ["en"],
   },
-  plugins: ["docusaurus-lunr-search","@docusaurus/theme-mermaid"],
+
+  plugins: [
+    [
+      require.resolve("docusaurus-lunr-search"),
+      {
+        enableHighlight: true,
+      },
+    ],
+    "@docusaurus/theme-mermaid",
+  ],
+
   presets: [
     [
       "classic",
       {
         docs: {
           sidebarPath: "./sidebars.ts",
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            "https://github.com/bcgov/nr-architecture-patterns-library/tree/main/patterns/",
         },
+        blog: false,
         theme: {
           customCss: "./src/css/custom.css",
         },
@@ -51,30 +65,76 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: "img/bc-gov-logo.svg",
+    image: "img/BCID_H_rgb_pos.png",
+    // announcementBar: {
+    //   id: "announcement",
+    //   content: "Lorem ipsum dolor sit amet",
+    //   backgroundColor: "#FEF1D8",
+    //   textColor: "#000000",
+    //   isCloseable: true,
+    // },
     navbar: {
-      style:"primary",
+      hideOnScroll: false,
       title: "NRM Architecture Patterns Library",
       logo: {
-        alt: "BCGov Logo",
-        src: "img/bc-gov-logo.svg",
+        alt: "Government of B.C.",
+        src: "img/BCID_H_rgb_pos.png",
+        srcDark: "img/BCID_H_rgb_rev.png",
       },
       items: [
+        // Uncomment if/when versioning is enabled
+        // {
+        //   type: "docsVersionDropdown",
+        //   position: "left",
+        //   // dropdownItemsAfter: [{ to: "/versions", label: "All versions" }],
+        //   dropdownActiveClassDisabled: true,
+        // },
         {
           type: "docSidebar",
           sidebarId: "tutorialSidebar",
-          position: "left",
+          position: "right",
           label: "Patterns",
         },
         {
-          type: "search",
-          position: "right", // this can be 'left' or 'right'
+          href: "https://github.com/bcgov/nr-architecture-patterns-library",
+          position: "right",
+          className: "header-github-logo",
+          "aria-label": "GitHub Repo",
         },
       ],
     },
     footer: {
-      style: "light",
-      copyright: `Copyright © ${new Date().getFullYear()} BCGov. Built with Docusaurus.`,
+      links: [
+        {
+          label: "Home",
+          href: "https://www2.gov.bc.ca/gov/content/home",
+        },
+        {
+          label: "About gov.bc.ca",
+          href: "https://www2.gov.bc.ca/gov/content/about-gov-bc-ca",
+        },
+        {
+          label: "Disclaimer",
+          href: "https://www2.gov.bc.ca/gov/content/home/disclaimer",
+        },
+        {
+          label: "Privacy",
+          href: "https://www2.gov.bc.ca/gov/content/home/privacy",
+        },
+        {
+          label: "Accessibility",
+          href: "https://www2.gov.bc.ca/gov/content/home/accessible-government",
+        },
+        {
+          label: "Copyright",
+          href: "https://www2.gov.bc.ca/gov/content/home/copyright",
+        },
+        {
+          label: "Contact us",
+          href: "https://www2.gov.bc.ca/gov/content/home/get-help-with-government-services",
+        }
+      ],
+      copyright: `© ${new Date().getFullYear()} Government of British Columbia. Built with Docusaurus.`,
     },
     prism: {
       theme: prismThemes.github,
