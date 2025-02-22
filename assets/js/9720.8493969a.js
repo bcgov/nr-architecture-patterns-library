9660dddbef2d073a00a1783d7c118aac79e85ee0
mishraomp <32200996+mishraomp@users.noreply.github.com>
2025-02-22 05:48:25 +0000

    Deploying to gh-pages from @ bcgov/nr-architecture-patterns-library@d4c74e055d46d0ad5383cb6c3395d299313fff7f 🚀

---
+++ assets/js/9720.8493969a.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunknr_architecture_patterns_library=self.webpackChunknr_architecture_patterns_library||[]).push([[9720],{9720:(r,e,t)=>{t.d(e,{createArchitectureServices:()=>c.S});var c=t(9936);t(9369)}}]);
